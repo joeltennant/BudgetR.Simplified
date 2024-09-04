e2816b6466fad1814dfeefaca18dc9106c3fcb50
Joel Tennant <joel_tennant@outlook.com>
2024-09-04 11:09:50 -0600

    Added Transactions page

---
+++ src/BudgetR.Simplified.Client.Domain/StateManagement/TransactionsByMonth.cs
@@ -0,0 +1,10 @@
+﻿namespace BudgetR.Simplified.Client.Domain.StateManagement;
+public class TransactionsByMonth : BaseState
+{
+    private readonly HttpClient Http;
+
+    public TransactionsByMonth(HttpClient http)
+    {
+        Http = http;
+    }
+}
